88e6fa27f50ee1d974b02614101cb7bafd57d65c
Mykhailo  Horbachenko <mykhailo.horbachenko@smartexe.com>
2022-10-13 00:31:01 +0300

    Initialize commit

---
+++ src/test_tradingview_ta.py
@@ -0,0 +1,7 @@
+from trading_view_wrapper.main_analisys import Analise
+
+
+analize_handler = Analise()
+for res in analize_handler():
+    print(res)
+
---
+++ src/trading_view_wrapper/exceptions.py
@@ -0,0 +1,10 @@
+class BrokenInterfaceAccessError(Exception):
+    """Use only `__call__` interface to access this class!"""
+
+
+class InvalidIntervalValueError(Exception):
+    """Raise when interval does not exist in `INTERVALS`"""
+
+
+class AccessToInitializeError(Exception):
+    """Raise when try to access before initialize data"""
